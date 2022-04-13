96428ab173d4ca312e33c987203332a04e1ab29a
Chantland <ericchantland@gmail.com>
2022-04-12 22:08:31 -0400

    Successful transition to class

--- Dice_Picture.py
+++ Dice_Picture.py
@@ -1,34 +1,24 @@
-# # import imageio as iio
-# # import visvis as vv
-# import cv2
-# import math
-# # from PIL import Image
-#
-#
-# # used to test
-# import numpy as np
+
+# requires the download of opencv (cv2) and math
 
 
-
-
-
-class dicepic():
-    def __init__(self, image): #Should this automatically give preset values if say this had "preset = True"
+class dicePic():
+    def __init__(self, image): # TODO: Should this automatically give preset values if say this had "preset = True"?
         import cv2
         self.img = cv2.imread(image)
 
-    def possible_blocks(Hor_x,Ver_y):
+    def possible_blocks(self):
         import numpy as np
         import math
 
+        hor_x,ver_y = self.img.shape[0:2] # TODO: I think I want this able function with an optional tuple argument, this might be removed later
 
-        num1 = Hor_x
-        num2 = Ver_y
 
-        x=0
         divisor = 2
         mod_list = []
-        greatest_mod = 1
+        greatest_mod = 1 # TODO: I think this is superfluous
+        num1,num2 = hor_x,ver_y #make copies so we can gradually reduce the number
+
         #for finding the greatest number than can go into both pixel lengths of the photo
         while divisor < min(num1, num2):
             if (num1 % divisor == 0) & (num2 % divisor == 0):
@@ -40,11 +30,6 @@ class dicepic():
             else:
                 divisor += 1
 
-            #to make sure it has an end point
-            x += 1
-            if x >200:
-                print('too many loops')
-                break
 
 
         mod_product_list = [] #for storing every number that can go into the two numbers given
@@ -57,15 +42,15 @@ class dicepic():
         mod_product_list.sort()
         pixel_mod_list = []
         for iDiv in mod_product_list:
-            pixel_mod_list.append([Hor_x/iDiv, Ver_y/iDiv])
+            pixel_mod_list.append([hor_x/iDiv, ver_y/iDiv])
         pixel_mod_list = np.array(pixel_mod_list)
 
         return pixel_mod_list
 
 
-    def show_im(image):
+    def showIm(self):
         import cv2
         #show image then run the following commands (or else it crashes)
-        cv2.imshow('image', image)
+        cv2.imshow('image', self.img)
         cv2.waitKey(0)  # show window until key press
         cv2.destroyAllWindows()  # then destroy
--- main.py
+++ main.py
@@ -6,3 +6,5 @@ reload(Dice_Picture);
 image = "J&E_Abby_Wedding.jpg"
 
 pic = Dice_Picture.dicepic(image)
+pic.possible_blocks()
+pic.show_im()
